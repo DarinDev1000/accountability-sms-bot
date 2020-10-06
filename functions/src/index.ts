4e4915e5101ed6e9205408adb05473b72062a1f9
Darin <darindev1000@gmail.com>
2020-10-06 03:23:18 -0700

    Experiment with Express It has a really weird auto routing based on function name. But it is not using my function name

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,74 +1,113 @@
 import * as functions from 'firebase-functions';
-const twilio = require("twilio");
+// const twilio = require("twilio");
 const MessagingResponse = require("twilio").twiml.MessagingResponse;
 
-const env = functions.config();
+// const env = functions.config();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+// // // Start writing Firebase Functions
+// // // https://firebase.google.com/docs/functions/typescript
+// //
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
 
-export const twilioTrial = functions.https.onRequest((request, response) => {
-  functions.logger.info("twilioTrial", { structuredData: true });
+// export const twilioTrial = functions.https.onRequest((request, response) => {
+//   functions.logger.info("twilioTrial", { structuredData: true });
 
-  const accountSid = env.twilio.accountsid; // Your Account SID from www.twilio.com/console
-  const authToken = env.twilio.authtoken; // Your Auth Token from www.twilio.com/console
+//   const accountSid = env.twilio.accountsid; // Your Account SID from www.twilio.com/console
+//   const authToken = env.twilio.authtoken; // Your Auth Token from www.twilio.com/console
 
-  const client = new twilio(accountSid, authToken);
+//   const client = new twilio(accountSid, authToken);
 
-  client.messages
-    .create({
-      body: "Hello from Node",
-      to: env.twilio.mynumber, // Text this number
-      from: env.twilio.twilionumber, // From a valid Twilio number
-    })
-    .then((message: any) => console.log(message.sid));
+//   client.messages
+//     .create({
+//       body: "Hello from Node",
+//       to: env.twilio.mynumber, // Text this number
+//       from: env.twilio.twilionumber, // From a valid Twilio number
+//     })
+//     .then((message: any) => console.log(message.sid));
   
-  response.send("Sent");
-});
+//   response.send("Sent");
+// });
 
-// Firebase has a native way to do express. Not like this
-export const twilioReceiveExpressTrial = functions.https.onRequest((request, response) => {
-  functions.logger.info("twilioReceiveExpressTrial", { structuredData: true });
+// // Firebase has a native way to do express. Not like this
+// export const twilioReceiveExpressTrial = functions.https.onRequest((request, response) => {
+//   functions.logger.info("twilioReceiveExpressTrial", { structuredData: true });
 
-  const http = require("http");
-  const express = require("express");
-  // const MessagingResponse = require("twilio").twiml.MessagingResponse;
+//   const http = require("http");
+//   const express = require("express");
+//   // const MessagingResponse = require("twilio").twiml.MessagingResponse;
 
-  const app = express();
+//   const app = express();
 
-  app.post("/sms", (req: any, res: any) => {
-    const twiml = new MessagingResponse();
+//   app.post("/sms", (req: any, res: any) => {
+//     const twiml = new MessagingResponse();
 
-    twiml.message("The Robots are coming! Head for the hills!");
+//     twiml.message("The Robots are coming! Head for the hills!");
 
-    res.writeHead(200, { "Content-Type": "text/xml" });
-    res.end(twiml.toString());
-  });
+//     res.writeHead(200, { "Content-Type": "text/xml" });
+//     res.end(twiml.toString());
+//   });
 
-  http.createServer(app).listen(1337, () => {
-    console.log("Express server listening on port 1337");
-  });
+//   http.createServer(app).listen(1337, () => {
+//     console.log("Express server listening on port 1337");
+//   });
 
   
-  response.send("Received");
-});
+//   response.send("Received");
+// });
 
-export const twilioReceiveMessageTrial = functions.https.onRequest((request, response) => {
-  functions.logger.info("twilioReceiveMessage", { structuredData: true });
+// export const twilioReceiveMessageTrial = functions.https.onRequest((request, response) => {
+//   functions.logger.info("twilioReceiveMessage", { structuredData: true });
 
-  const twiml = new MessagingResponse();
-  twiml.message("The Robots are coming! Head for the hills 2!");
+//   const twiml = new MessagingResponse();
+//   twiml.message("The Robots are coming! Head for the hills 2!");
 
-  response.set({ "Content-Type": "text/xml" });
-  response.send(twiml.toString());
-});
+//   response.set({ "Content-Type": "text/xml" });
+//   response.send(twiml.toString());
+// });
 
-export const twilioReceiveAndSend = functions.https.onRequest((request, response) => {
+// export const twilioReceiveAndSend = functions.https.onRequest((request, response) => {
+//   functions.logger.info("twilioReceiveAndSend", { structuredData: true });
+
+//   console.log('body: ', request.body.Body);
+  
+//   const twiml = new MessagingResponse();
+//   if (request.body.Body) {
+//     twiml.message(request.body.Body);
+//   } else {
+//     response.status(400).end();
+//     throw console.error('no body received');
+//   }
+  
+//   console.log('response: ', twiml.toString());
+//   response.set({ "Content-Type": "text/xml" });
+//   response.status(200).send(twiml.toString());
+// });
+
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
+
+// Add middleware to authenticate requests
+// app.use(myMiddleware);
+
+// build multiple CRUD interfaces:
+// app.get("/:id", (req: any, res: any) => res.send(Widgets.getById(req.params.id)));
+// app.post("/", (req: any, res: any) => res.send(Widgets.create()));
+// app.put("/:id", (req: any, res: any) => res.send(Widgets.update(req.params.id, req.body)));
+// app.delete("/:id", (req: any, res: ) => res.send(Widgets.delete(req.params.id)));
+// app.get("/", (req: any, res: any) => res.send(Widgets.list()));
+
+app.post("/", (req: any, res: any) => res.send(twilioReceiveAndSend(req, res)));
+
+const twilioReceiveAndSend = (request: any, response: any) => {
   functions.logger.info("twilioReceiveAndSend", { structuredData: true });
 
   console.log('body: ', request.body.Body);
@@ -84,4 +123,7 @@ export const twilioReceiveAndSend = functions.https.onRequest((request, response
   console.log('response: ', twiml.toString());
   response.set({ "Content-Type": "text/xml" });
   response.status(200).send(twiml.toString());
-});
+}
+
+// Expose Express API as a single Cloud Function:
+exports.widgets = functions.https.onRequest(app);
