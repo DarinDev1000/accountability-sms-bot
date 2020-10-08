65bd8e21975bf4b07591383cb08b81638bf006fd
Darin <darindev1000@gmail.com>
2020-10-08 06:40:02 -0700

    Mocha woring!

--- functions/src/index.test.ts
+++ functions/src/index.test.ts
@@ -1,6 +1,5 @@
-// Online version setup
-// // At the top of test/index.test.js
-// const test = require('firebase-functions-test')({
+// // Online version setup
+// const firebaseTest = require('firebase-functions-test')({
 //   databaseURL: 'https://my-project.firebaseio.com',
 //   storageBucket: 'my-project.appspot.com',
 //   projectId: 'my-project',
@@ -12,7 +11,7 @@ const assert = chai.assert;
 // Sinon is a library used for mocking or verifying function calls in JavaScript.
 const sinon = require('sinon');
 // For not exported functions
-// const rewire = require('rewire');
+const rewire = require('rewire');
 
 // Require firebase-admin so we can stub out some of its methods.
 const admin = require('firebase-admin');
@@ -32,8 +31,8 @@ const env = functions.config();
 const adminInitStub = sinon.stub(admin, 'initializeApp');
 
 // Now we can require index.js and save the exports inside a namespace called myFunctions.
-const index = require('./index'); // Replacing this with rewire for non exported functions
-// const index = rewire('./index.js'); // This allow me to not have to export functions
+// const index = require('./index'); // Replacing this with rewire for non exported functions
+const index = rewire('./index'); // This allow me to not have to export functions
 
 // Basic test
 describe('basicTest', () => {
@@ -43,18 +42,18 @@ describe('basicTest', () => {
 });
 
 // describe('handleIncomingMessage', () => {
-//   const handleIncomingMessage = myFunctions.__get__('handleIncomingMessage');
+//   const handleIncomingMessage = index.__get__('handleIncomingMessage');
 //   it('should be 6', async () => {
 //     // "Wrap" the basicTest function from index.js
-//     // const wrapped = test.wrap(myFunctions.handleIncomingMessage);
+//     // const wrapped = test.wrap(index.handleIncomingMessage);
 //     // return assert.equal(wrapped(), true);
 //     return assert.equal(await handleIncomingMessage("{IncomingMessage: {body: { Body: 'the body'}}}"), 'output')
 //   });
 // });
 
-// describe('helpCommand', () => {
-//   const helpCommand = myFunctions.__get__('helpCommand');
-//   it('should return default help', async () => {
-//     return assert.typeOf(await helpCommand('help commands'), 'string');
-//   });
-// });
+describe('helpCommand', () => {
+  const helpCommand = index.__get__('helpCommand');
+  it('should return default help', async () => {
+    return assert.typeOf(await helpCommand('help commands'), 'string');
+  });
+});
